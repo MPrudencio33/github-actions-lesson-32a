28716484935a9e4763e8cc6ed008093f4ece565c
zanjero <zanjero3333@gmail.com>
2025-10-27 01:36:27 +0800

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-provider "aws" { 
-  region = "us-east-1" 
-} 
- 
-terraform { 
-  backend "s3" { 
-    bucket = "sctp-core-tfstate" 
-    key    = "marlon.tfstate"  #Change this 
-    region = "ap-southeast-1" 
-  } 
-} 
- 
-data "aws_caller_identity" "current" {} 
- 
-locals { 
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" 
-  account_id  = "${data.aws_caller_identity.current.account_id}" 
-} 
- 
-resource "aws_s3_bucket" "s3_tf" { 
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-core-tfstate"
+    key    = "marlon.tfstate" #Change this 
+    region = "ap-southeast-1"
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  name_prefix = lower(split("/", "${data.aws_caller_identity.current.arn}")[1])
+  account_id  = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "s3-tf" {
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
