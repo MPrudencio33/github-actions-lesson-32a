3fb67023220fcfedc32ed26a93d218fced006f36
zanjero <zanjero3333@gmail.com>
2025-10-27 18:42:43 +0800

    updated file

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ data "aws_caller_identity" "current" {}
 
 locals {
   name_prefix = split("/", data.aws_caller_identity.current.arn)[1]  
-  # lower(split("/", "${data.aws_caller_identity.current.arn}")[1])
   account_id  = data.aws_caller_identity.current.account_id
 }
 
